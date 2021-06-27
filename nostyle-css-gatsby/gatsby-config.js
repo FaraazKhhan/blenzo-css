317a01d45f3a7d133eb7ecd021ad266706d34776
FaraazKhhan <codefaraaz@gmail.com>
2021-06-27 18:23:29 +0530

    favicon issue fixed

--- nostyle-css-gatsby/gatsby-config.js
+++ nostyle-css-gatsby/gatsby-config.js
@@ -19,18 +19,6 @@ module.exports = {
         path: `${__dirname}/src/documentation`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `NOStyleCSS`,
-        short_name: `NOStyleCSS`,
-        start_url: `/`,
-        icon: `src/images/favicon.svg`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-      },
-    },
     `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
   ],
