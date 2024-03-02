d2e9e07d4811fc3ce24b0a97b3bd84e87ce629d0
Faraaz Khan <codefaraaz@gmail.com>
2024-03-02 15:02:27 +0530

    fixed(core): Switch defaultChecked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -78,7 +78,7 @@ function Navbar() {
               <Link to="/about">About</Link>
             </li>
             <li className="navbar-link">
-              <Switch icon={themeIcon} handler={toggleTheme} initialChecked={theme} />
+              <Switch icon={themeIcon} handler={toggleTheme} defaultChecked={theme === 'dark' ? true : false} />
             </li>
           </ul>
         </div>
--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import "../../static/css/switch.css"
 
-function Switch({ icon = '', handler = '', initialChecked = false }) {
+function Switch({ icon = '', handler = '', defaultChecked = false }) {
     const changeIcon = (e) => {
-        console.debug("Switch.js handler triggered::Props ", { icon, initialChecked })
+        console.debug("Switch.js handler triggered::Props ", { icon, defaultChecked })
         if (handler) handler();
     }
 
     return (
         <label className="switch">
-            <input type="checkbox" onChange={changeIcon} checked={initialChecked} />
+            <input type="checkbox" onChange={changeIcon} defaultChecked={defaultChecked} />
             <span className="slider round"></span>
         </label>
     )
